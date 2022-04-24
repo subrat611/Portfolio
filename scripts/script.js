e0be8afee6ee80a7670db64dcc0990ae88db536e
subrat k jena <subratj611@gmail.com>
2022-04-24 20:00:20 +0530

    overflow image done and resposive modified 390->412

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,11 @@ const mob_menu = document.querySelector('.mob-menu');
 hambar.addEventListener('click',()=>{
     hambar.classList.toggle('active');
     mob_menu.classList.toggle('active');
+    
+    // overflow toggle
+    if(document.body.style.overflow != 'hidden'){
+        document.body.style.overflow = 'hidden';
+    }else {
+        document.body.style.overflow = 'auto';
+    }
 })
